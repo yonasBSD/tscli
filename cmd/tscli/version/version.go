6ea54dd23597a4e4c27bd8aad72571639d20caf1
Lee Briggs <lee@leebriggs.co.uk>
2025-06-20 21:41:33 -0700

    Fix version import package
    Updates #25
    
    Signed-off-by: Lee Briggs <lee@leebriggs.co.uk>

--- cmd/tscli/version/version.go
+++ cmd/tscli/version/version.go
@@ -7,7 +7,7 @@ import (
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/pulumi/pulumictl/pkg/gitversion"
-	"github.com/pulumi/pulumictl/pkg/version"
+	"github.com/jaxxstorm/tscli/pkg/version"
 	"github.com/spf13/cobra"
 )
 
