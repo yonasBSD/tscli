11a82d4c127a21124b4e257e20fb88f5c57ff3f1
Lee Briggs <lee@leebriggs.co.uk>
2025-06-30 21:48:08 -0700

    switch to vers for version calculation
    Signed-off-by: Lee Briggs <lee@leebriggs.co.uk>

--- cmd/tscli/version/version.go
+++ cmd/tscli/version/version.go
@@ -2,12 +2,9 @@ package version
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/jaxxstorm/tscli/pkg/version"
-	"github.com/pulumi/pulumictl/pkg/gitversion"
+	"github.com/jaxxstorm/vers"
 	"github.com/spf13/cobra"
 )
 
@@ -19,26 +16,23 @@ func Command() *cobra.Command {
 		RunE: func(*cobra.Command, []string) error {
 
 			v := version.Version
-			// If we haven't set a version with linker flags, use this tool to get the version
+			// If we haven't set a version with linker flags, calculate from git
 			if v == "" {
-				commitish := "HEAD"
-				workingDir, err := os.Getwd()
-				if err != nil {
-					return fmt.Errorf("error obtaining working directory: %w", err)
-				}
-				// Open repository
-				repo, err := git.PlainOpenWithOptions(workingDir, &git.PlainOpenOptions{EnableDotGitCommonDir: true})
+				repo, err := vers.OpenRepository(".")
 				if err != nil {
 					return fmt.Errorf("error opening repository: %w", err)
 				}
 
-				version, err := gitversion.GetLanguageVersions(repo, plumbing.Revision(commitish), true,
-					"" /*isPrerelease*/, false)
+				opts := vers.Options{
+					Repository: repo,
+					Commitish:  "HEAD",
+				}
+
+				versions, err := vers.Calculate(opts)
 				if err != nil {
 					return fmt.Errorf("error calculating version: %w", err)
 				}
-
-				v = version.SemVer
+				v = versions.SemVer
 			}
 			fmt.Println(v)
 			return nil
--- go.mod
+++ go.mod
@@ -6,8 +6,7 @@ toolchain go1.24.4
 
 require (
 	github.com/charmbracelet/lipgloss v1.1.0
-	github.com/go-git/go-git/v5 v5.16.2
-	github.com/pulumi/pulumictl v0.0.49
+	github.com/jaxxstorm/vers v0.0.0-20250701044105-e3c5a13decb4
 	github.com/spf13/cobra v1.9.1
 	github.com/spf13/pflag v1.0.6
 	github.com/spf13/viper v1.20.1
@@ -33,6 +32,7 @@ require (
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.6.2 // indirect
+	github.com/go-git/go-git/v5 v5.16.2 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
